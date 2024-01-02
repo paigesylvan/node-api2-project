76e6db404279650f83e1de58aa659689dce64b7f
PaigeSylvan <paigesylvan@gmail.com>
2024-01-02 15:52:14 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require ('express')
-const postsRouter = require('./posts/')
-const server = require()
+const postsRouter = require('./posts/posts-router')
+const server = express()
 
 server.use(express.json())
 
